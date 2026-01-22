9090c0a7617f8657a835b164763b1854b46b4b1e
fasilofficial <muhammedfasilofficial@gmail.com>
2026-01-22 14:28:13 +0530

    update api/helpers/bitrix.php

--- api/helpers/bitrix.php
+++ api/helpers/bitrix.php
@@ -6,12 +6,19 @@ function bitrixRequest($method, $params = [])
     $ch = curl_init($url);
     curl_setopt_array($ch, [
         CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_POST => true,
-        CURLOPT_POSTFIELDS => http_build_query($params)
+        CURLOPT_POST           => true,
+        CURLOPT_POSTFIELDS     => http_build_query($params),
+        CURLOPT_TIMEOUT        => 30,
     ]);
 
     $response = curl_exec($ch);
-    curl_close($ch);
+
+    if ($response === false) {
+        return [
+            'error' => true,
+            'message' => curl_error($ch),
+        ];
+    }
 
     return json_decode($response, true);
 }
