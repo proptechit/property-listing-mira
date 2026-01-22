4db761f8478b99b8b52f4070e9ebf98df67ea693
fasilofficial <muhammedfasilofficial@gmail.com>
2026-01-22 13:38:03 +0530

    Update api/index.php

--- api/index.php
+++ api/index.php
@@ -1,7 +1,37 @@
 <?php
 require 'config.php';
 
+/**
+ * ======================
+ * CORS CONFIG (localhost)
+ * ======================
+ */
+$allowedOrigins = [
+    'http://localhost',
+    'http://127.0.0.1',
+    'http://localhost:3000',
+    'http://localhost:5173',
+    'http://localhost:8080'
+];
+
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+
+if (in_array($origin, $allowedOrigins, true)) {
+    header("Access-Control-Allow-Origin: {$origin}");
+    header('Access-Control-Allow-Credentials: true');
+}
+
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-API-KEY');
 header('Content-Type: application/json');
+
+/**
+ * Handle preflight request
+ */
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 
 $method = $_SERVER['REQUEST_METHOD'];
 
